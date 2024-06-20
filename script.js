540b67f7b4eb7e725e9c14f8a5be04e588433b41
unknown <jeniferrefinej0@gmail.com>
2024-06-20 23:50:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+let navbarscrolling =  document.querySelector(".navbar")
+window.addEventListener("scroll",()=>{
+    if(window.scrollY >= 0){
+        navbarscrolling.style.backgroundColor= "black"
+
+    }
+    else{
+        navbarscrolling.style.backgroundColor = "black"
+    }
+})
+// document.addEventListener('DOMContentLoaded', function() {
+//     const filterButtons = document.querySelectorAll('.filter-btn');
+//     const projectItems = document.querySelectorAll('.project-item');
+
+//     filterButtons.forEach(button => {
+//         button.addEventListener('click', () => {
+//             const filter = button.getAttribute('data-filter');
+
+//             projectItems.forEach(item => {
+//                 if (filter === 'all') {
+//                     item.classList.add('show');
+//                 } else if (item.classList.contains(filter)) {
+//                     item.classList.add('show');
+//                 } else {
+//                     item.classList.remove('show');
+//                 }
+//             });
+//         });
+//     });
+
+  
+// });
+
+//page animation
+function applyAnimationOnVisible(elementSelector, animationClass) {
+    window.addEventListener("scroll", () => {
+        document.querySelectorAll(elementSelector).forEach((element) => {
+            let rect = element.getBoundingClientRect();
+            let isVisible = rect.top < window.innerHeight && rect.bottom >= 0;
+            if (isVisible) {
+                element.classList.add(animationClass);
+            }
+        });
+    });
+}
+
+// Apply the animation to the specific elements
+applyAnimationOnVisible(".about .large-text", "animationtext");
+applyAnimationOnVisible(".skill-container .large-text", "animationtext");
+applyAnimationOnVisible(".project-container .large-text", "animationtext");
+applyAnimationOnVisible(".contact-container .large-text", "animationtext");
+
+applyAnimationOnVisible(".contact-container .large-text","animationtext")
+
+
